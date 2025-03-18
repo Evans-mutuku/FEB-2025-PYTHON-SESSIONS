19449ac7946d5b0da199615f2a565176d8a2fe68
Evans-mutuku <evansnyamai98@gmail.com>
2025-03-18 18:31:53 +0300

    week 2 of python

---
+++ WEEK 2/dictionary.py
@@ -0,0 +1,29 @@
+students = {
+    "name": "Evans",
+    "age": 24,
+    "gender": "male",
+    "is_student": True,
+    "courses": ["Math", "Science", "English"],
+
+    "my_name" : {
+        "first_name": "Evans",
+        "last_name": "Kinyua"
+    }
+}
+
+# students["height"] = 2.56
+
+# print(students)
+# print(students["height"])
+
+
+
+
+capital_cities = {
+    "Kenya": ["Nairobi", "Mombasa", "Kisumu"],
+    "Uganda": "Kampala",
+    "South Africa": "Johannesburg",
+}
+
+print(capital_cities)
+print(capital_cities["South Africa"])
---
+++ WEEK 2/sets.py
@@ -0,0 +1,17 @@
+students_ids = {0, 2, 3, 4, False, "John"}
+# print(students_ids)
+
+
+
+
+
+
+
+
+
+
+
+
+employees_ids = {1,  2, 4, 5,8}
+print(employees_ids)
+employees_ids.add(89)
